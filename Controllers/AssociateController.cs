fcb093b897f8e0a92f8634173e8d8fa0bbb6335c
Natarajan <nataraj.itsolution@gmail.com>
2024-06-29 16:05:23 +0530

    Add project files.

---
+++ Controllers/AssociateController.cs
@@ -0,0 +1,91 @@
+﻿using learndotnet.Repos;
+using learndotnet.Repos.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace learndotnet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssociateController : ControllerBase
+    {
+        private readonly LearndataContext _context;
+        public AssociateController(LearndataContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet("Getall")]
+        public async Task<IActionResult> Getall()
+        {
+            var _data = await this._context.TblCustomers.ToListAsync();
+            if (_data.Any())
+            {
+                return Ok(_data);
+            }
+            else
+            {
+                return NotFound();
+            }
+            
+        }
+
+        [HttpGet("Getbycode")]
+        public async Task<IActionResult> Getbycode(string code)
+        {
+            var _data = await this._context.TblCustomers.FindAsync(code);
+            if (_data != null)
+            {
+                return Ok(_data);
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create(TblCustomer tblCustomer)
+        {
+            await this._context.TblCustomers.AddAsync(tblCustomer);
+            await this._context.SaveChangesAsync();
+            return Ok(await Getall());
+
+        }
+
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update(TblCustomer tblCustomer,string code)
+        {
+            var _data= await this._context.TblCustomers.FindAsync(code);
+            if(_data!=null)
+            {
+                _data.Name = tblCustomer.Name;
+                _data.Email=tblCustomer.Email;
+                _data.Creditlimit = tblCustomer.Creditlimit;
+                _data.Taxcode = tblCustomer.Taxcode;
+                _data.Phone = tblCustomer.Phone;
+            }
+            await this._context.SaveChangesAsync();
+            return Ok(await Getall());
+
+        }
+
+        [HttpDelete("Remove")]
+        public async Task<IActionResult> Remove(string code)
+        {
+            var _data = await this._context.TblCustomers.FindAsync(code);
+            if (_data != null)
+            {
+                this._context.TblCustomers.Remove(_data);
+            }
+            await this._context.SaveChangesAsync();
+            return Ok(await Getall());
+
+        }
+
+    }
+}
+ 
---
+++ Repos/LearndataContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using learndotnet.Repos.Models;
+
+namespace learndotnet.Repos;
+
+public partial class LearndataContext : DbContext
+{
+    public LearndataContext()
+    {
+    }
+
+    public LearndataContext(DbContextOptions<LearndataContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TblCustomer> TblCustomers { get; set; }
+
+    public virtual DbSet<TblMenu> TblMenus { get; set; }
+
+    public virtual DbSet<TblOtpManager> TblOtpManagers { get; set; }
+
+    public virtual DbSet<TblProduct> TblProducts { get; set; }
+
+    public virtual DbSet<TblProductimage> TblProductimages { get; set; }
+
+    public virtual DbSet<TblPwdManger> TblPwdMangers { get; set; }
+
+    public virtual DbSet<TblRefreshtoken> TblRefreshtokens { get; set; }
+
+    public virtual DbSet<TblRole> TblRoles { get; set; }
+
+    public virtual DbSet<TblRolepermission> TblRolepermissions { get; set; }
+
+    public virtual DbSet<TblTempuser> TblTempusers { get; set; }
+
+    public virtual DbSet<TblUser> TblUsers { get; set; }
+
+   
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TblTempuser>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("tbl_tempuser1");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
